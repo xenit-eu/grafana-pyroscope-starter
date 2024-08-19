4781617be441135a75f1442c8a19af652d6df3f5
Thijs Lemmens <thijs.lemmens@xenit.eu>
2024-08-19 17:28:36 +0200

    ACC-1524 Add publication plugin. Fix autoconfiguration setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
 	`java-library`
+	`maven-publish`
+	`signing`
 	id("io.freefair.lombok") version "8.6"
 }
 
@@ -27,4 +29,74 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("library") {
+			from(components["java"])
+
+			pom {
+				url.set("https://github.com/xenit-eu/grafana-pyroscope-starter")
+				name.set(project.name)
+				description.set(project.description)
+
+				scm {
+					connection.set("scm:git:git@github.com:xenit-eu/grafana-pyroscope-starter.git")
+					developerConnection.set("scm:git:git@github.com:xenit-eu/grafana-pyroscope-starter.git")
+					url.set("https://github.com/xenit-eu/grafana-pyroscope-starter.git")
+				}
+
+				developers {
+					developer {
+						name.set("XeniT")
+						organization.set("XeniT Solutions NV")
+					}
+				}
+
+				licenses {
+					license {
+						name.set("The Apache License, Version 2.0")
+						url.set("https://www.apache.org/licenses/LICENSE-2.0")
+					}
+				}
+			}
+
+			versionMapping {
+				usage("java-api") {
+					fromResolutionResult()
+				}
+				usage("java-runtime") {
+					fromResolutionResult()
+				}
+			}
+		}
+	}
+
+	repositories {
+		maven {
+			name = if (project.version.toString().endsWith("-SNAPSHOT")) {
+				"sonatypeSnapshots"
+			} else {
+				"sonatypeMavenCentral"
+			}
+
+			url = uri(
+				if (project.version.toString().endsWith("-SNAPSHOT")) {
+					"https://s01.oss.sonatype.org/content/repositories/snapshots/"
+				} else {
+					"https://s01.oss.sonatype.org/service/local/"
+				}
+			)
+
+			credentials {
+				username = project.findProperty("sonatype_username") as String?
+				password = project.findProperty("sonatype_password") as String?
+			}
+		}
+	}
+}
+
+tasks.named("check") {
+	dependsOn("checkMavenCentralRequirements")
 }
